5ce17edb76b132953c76f2270ff55d407e2834a4
Paul Geeser <commit@pauldev.sh>
2025-01-27 17:32:35 +0100

    fixed build issue

--- apps/frontend/src/app/diary/new/page.tsx
+++ apps/frontend/src/app/diary/new/page.tsx
@@ -2,15 +2,17 @@ import { getDiary } from '@/api/missionboard';
 import DiaryNew from '../src/components/layout/DiaryNew';
 import { auth } from "@/auth/server";
 
-export default async function NewDiaryPage({ searchParams }: { searchParams: { edit?: string } }) {
+export default async function NewDiaryPage({ searchParams }: { searchParams: Promise<{ edit?: string }> }) {
 	const session = await auth();
 	let initialDiary;
 	
-	if (searchParams.edit) {
-		const [_, accountId, __, diaryId] = searchParams.edit.split('/');
-		const response = await getDiary(accountId, diaryId);
-		initialDiary = response.data;
+	if ((await searchParams).edit) {
+		const [_, accountId, __, diaryId] = (await searchParams).edit?.split('/') ?? [];
+		if (accountId && diaryId) {
+			const response = await getDiary(accountId, diaryId);
+			initialDiary = response.data;
+		}
 	}
-	
+
 	return <DiaryNew session={session} initialDiary={initialDiary} />;
 }
